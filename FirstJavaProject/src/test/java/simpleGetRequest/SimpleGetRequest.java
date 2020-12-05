98920a5e1b986c27ad9ef17871cbf2178885c490
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-05 11:26:51 +0100

    Added several tests.

--- FirstJavaProject/src/test/java/simpleGetRequest/SimpleGetRequest.java
+++ FirstJavaProject/src/test/java/simpleGetRequest/SimpleGetRequest.java
@@ -1,6 +1,7 @@
 package simpleGetRequest;
 
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -10,6 +11,39 @@ public class SimpleGetRequest {
 
     @Test
     public void requestUsZipCode90210_checkPlaceNameInResponseBody_expectBeverlyHills() {
+
+        given().when().
+                get("http://zippopotam.us/us/90210").
+                then().
+                assertThat().body("places[0].'place name'", equalTo("Beverly Hills"));
+
+    }
+
+    @Test
+    public void statusCode() {
+
+        given().
+                log().all().
+                when().
+                get("http://zippopotam.us/us/90210").
+                then().
+                log().body().
+                assertThat().statusCode(200);
+
+    }
+
+    @Test
+    public void header_contenttypes() {
+
+        given().when().
+                get("http://zippopotam.us/us/90210").
+                then().
+                assertThat().contentType(ContentType.JSON);
+
+    }
+
+    @Test
+    public void bodyChecks() {
 
         given().when().
                 get("http://zippopotam.us/us/90210").
