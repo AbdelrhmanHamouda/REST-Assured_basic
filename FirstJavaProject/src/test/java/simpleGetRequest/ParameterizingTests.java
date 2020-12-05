e07bd4f0191542bc257bcedcf54053cf7b57eae7
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-05 11:44:35 +0100

    Added several new test.

---
+++ FirstJavaProject/src/test/java/simpleGetRequest/ParameterizingTests.java
@@ -0,0 +1,2 @@
+package simpleGetRequest;public class ParameterizingTests {
+}
--- FirstJavaProject/src/test/java/simpleGetRequest/SimpleGetRequest.java
+++ FirstJavaProject/src/test/java/simpleGetRequest/SimpleGetRequest.java
@@ -5,7 +5,7 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 
 public class SimpleGetRequest {
 
@@ -49,6 +49,36 @@ public class SimpleGetRequest {
                 get("http://zippopotam.us/us/90210").
                 then().
                 assertThat().body("places[0].'place name'", equalTo("Beverly Hills"));
+
+    }
+
+    @Test
+    public void bodyChecksWithHasItem() {
+
+        given().when().
+                get("http://zippopotam.us/us/90210").
+                then().
+                assertThat().body("places.'place name'", hasItem("Beverly Hills"));
+
+    }
+
+    @Test
+    public void bodyChecksWithHasItemWithNot() {
+
+        given().when().
+                get("http://zippopotam.us/us/90210").
+                then().
+                assertThat().body("places.'place name'", not(hasItem("NY")));
+
+    }
+
+    @Test
+    public void bodyChecksWithHasIsize() {
+
+        given().when().
+                get("http://zippopotam.us/us/90210").
+                then().
+                assertThat().body("places.'place name'", hasSize(1));
 
     }
 }
